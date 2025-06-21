8fa7e9922ca87982d0a965c75e83db2b674c09ae
旧 <33697555+2233244406@users.noreply.github.com>
2025-06-21 15:59:44 +0800

    Create 麻豆VLOG.js

---
+++ 麻豆VLOG.js
@@ -0,0 +1,12 @@
+{
+  "作者": "MaDouV",
+  "站名": "麻豆VLOG",
+  "主页url": "http://mdvlog88.com/",
+  "简介": "<div class=\stui-vodlist__detail\" style=*>&&</div>",
+  "影片状态": "状态：&&</p>",
+  "搜索url": "/index.php/vod/search.html?wd={wd}&submit=",
+  "线路数组": "<h3&&/h3>",
+  "线路标题": "MeowTV-+src=*>&&<",
+  "分类url": "http://mdvlog88.com/index.php/vod/search.html?wd=91%E8%8C%84%E5%AD%90",
+  "分类": "91"
+}
